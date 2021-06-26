b19e1d8fa4674467a18f9ac2796ac61ac4378c34
Michelle Simek <michellesimek@michelles-mbp.attlocal.net>
2021-06-26 13:17:27 -0500

    plotted data points on chart

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -22,7 +22,7 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // create svg wrapper and append an SCG group that will hold chart
 var svg = d3
-  .select("body")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -33,17 +33,77 @@ var chartGroup = svg.append("g")
 // import data
 d3.csv("assets/data/data.csv").then(data => {
     // console log to make sure data loads in
-    console.log(data)
+    console.log([data])
+    // let chartData = {};
+    // let stateAbbrev = [];
     
     data.forEach(data => {
-        console.log(data.state);
+      data.poverty = +data.poverty;
+      data.healthcareLow = +data.healthcareLow;
+      data.abbr = +data.abbr;
+        // let domain = d3.extent(data)
+        // console.log(domain)
+
+        console.log(data.poverty);
+        console.log(data.healthcareLow);
+        console.log(data.abbr);
     })
 
-    // Add X Axis
+    // create scales
+    var xLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.poverty)])
+      .range([0, width]);
 
-    // Add Y Axis
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.healthcareLow)])
+      .range([height,0]);
 
-    // Add markers for data points
-}).catch(function(error) {
+    // create axes
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // append axes
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // create and append circles
+    var circlesGroup = chartGroup.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcareLow))
+      .attr("r", "15")
+      .attr("fill", "blue")
+      .attr("opacity", "0.25")
+      .attr("stroke-width", "1")
+      .attr("stroke", "black");
+
+    /* Create the text for each block */
+    // circlesGroup.append("text")
+    //   .text(data => {return data.abbr})
+    // });
+
+     // Create axes labels
+     chartGroup.append("text")
+     .attr("transform", "rotate(-90)")
+     .attr("y", 0 - margin.left)
+     .attr("x", 0 - (height / 1.5))
+     .attr("dy", "1em")
+     .attr("class", "axisText")
+     .text("Lacks HealthCare");
+
+    chartGroup.append("text")
+     .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+     .attr("class", "axisText")
+     .text("Poverty");
+
+  }).catch(function(error) {
     console.log(error);
 });
+
+  
